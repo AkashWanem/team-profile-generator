16d8fc42c32ea69b5731653a62ee43a743a46b5a
Akash Limbu <akashwanem@hotmail.com>
2022-03-06 14:35:52 +0000

    pushed all roles into html array

--- src/teamTemplate.js
+++ src/teamTemplate.js
@@ -59,6 +59,25 @@ const constructTeam = team => {
                 `;
     };
 
-    
+    // pushing individual role to html array
+    const html = [];
+
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        .join("")
+    );
+
+    return html.join("");
+
 
 }
