4c772677873569b9863e8ff28b8d88343721cb53
Akash Limbu <akashwanem@hotmail.com>
2022-03-06 14:20:42 +0000

    added engineer html

--- src/teamTemplate.js
+++ src/teamTemplate.js
@@ -20,5 +20,24 @@ const constructTeam = team => {
         </div>
                 `;
     };
-     
+
+    // construct engineer html
+    const constructEngineer = engineer => {
+        return `
+        <div class="card text-white bg-info m-3" style="max-width: 18rem;">
+            <div class="card-header">
+                <h2 class='card-title'>${engineer.getName()}</h2>
+                <p class='card-title'><i class="fa-solid fa-glasses"></i>${engineer.getRole()}</p>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                </ul>
+            </div>
+        </div>
+                `;
+    };
+
 }
